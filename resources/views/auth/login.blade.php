b8b9f64dfc2776b35303257d59ca65987b663384
Lahiru <lmillangoda@gmail.com>
2018-12-29 10:23:52 +0530

    auth interface redesign

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,91 +1,66 @@
-@extends('layouts.users')
+@extends('layouts.auth')
 
 @section('content')
 
-<section class="section section-shaped section-lg">
-    <div class="shape shape-style-1 bg-gradient-default">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="container pt-lg-md">
-      <div class="row justify-content-center">
-        <div class="col-lg-5">
-          <div class="card bg-secondary shadow border-0">
-            <div class="card-body px-lg-5 py-lg-5">
+<form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="card-header text-center">
+          <div class="logo-container">
+            <img src="{{URL::asset('assets/img/logo.png')}}" alt="">
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="input-group no-border input-lg">
+            <div class="input-group-prepend">
+              <span class="input-group-text">
+                <i class="now-ui-icons users_circle-08"></i>
+              </span>
+            </div>
+            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder ="E-Mail"required autofocus>
 
-              <div class="text-center text-muted mb-4">
-                <small>Login with credentials</small>
-              </div>
-              <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="form-group mb-3">
-                  <div class="input-group input-group-alternative">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                    </div>
-                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Email" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="input-group input-group-alternative">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
-                    </div>
-                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Password" name="password" required>
+            @if ($errors->has('email'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
+          </div>
+          <div class="input-group no-border input-lg">
+            <div class="input-group-prepend">
+              <span class="input-group-text">
+                <i class="now-ui-icons ui-1_lock-circle-open"></i>
+              </span>
+            </div>
+            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder = "Password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                  </div>
-                </div>
-                <div class="custom-control custom-control-alternative custom-checkbox">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+          </div>
+          <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                  <span class="form-check-sign"></span>
+                  Remember me
+                </label>
+              </div>
 
-                    <label class="form-check-label" for="remember">
-                        {{ __('Remember Me') }}
-                    </label>
-                </div>
-                </div>
-                <div class="text-center">
-                  <button type="submit" class="btn btn-primary my-4">Login</button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div class="row mt-3">
-            <div class="col-6">
-              <a href="{{ route('password.request') }}" class="text-light">
-                <small>Forgot password?</small>
-              </a>
-            </div>
-            <div class="col-6 text-right">
-              <a href="{{ route('register') }}" class="text-light">
-                <small>Create new account</small>
-              </a>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="col-md-8 col-md-offset-4">
-              <a href="{{url('/login/facebook')}}" class="btn btn-primary">Login with Facebook</a>
-            </div>
         </div>
+        <div class="card-footer text-center">
+                <button class="btn btn-primary" type="submit">Login</button>
         </div>
-      </div>
-    </div>
-  </section>
+          <div class="pull-left">
+            <h6>
+              <a href="/register" class="link">Register</a>
+            </h6>
+          </div>
+          <div class="pull-right">
+                <h6>
+                  <a href="{{ route('password.request') }}" class="link">Forgot Your Password?</a>
+                </h6>
+              </div>
+              
+      </form>
+
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,107 +1,104 @@
-@extends('layouts.Users')
+@extends('layouts.auth')
 
 @section('content')
-<section class="section section-shaped section-lg">
-    <div class="shape shape-style-1 bg-gradient-default">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="container pt-lg-md">
-      <div class="row justify-content-center">
-        <div class="col-lg-5">
-          <div class="card bg-secondary shadow border-0">
-            <div class="card-body px-lg-5 py-lg-5">
-              <div class="text-center text-muted mb-4">
-                <small>Registration</small>
-              </div>
-              <form method="POST" action="{{ route('register') }}">
-                @csrf
-                <div class="form-group row">
-                    <div class="input-group input-group-alternative mb-3">
-                      <div class="input-group-prepend">
-                        <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
-                      </div>
-                       <input id="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder ="Name" name="name" value="{{ old('name') }}" type="text" required autofocus>
-  
-                                  @if ($errors->has('name'))
-                                      <span class="invalid-feedback" role="alert">
-                                          <strong>{{ $errors->first('name') }}</strong>
-                                      </span>
-                                  @endif
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                        <div class="input-group input-group-alternative mb-3">
-                          <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                          </div>
-                                      <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" placeholder = "E-mail" value="{{ old('email') }}" required>
-      
-                                      @if ($errors->has('email'))
-                                          <span class="invalid-feedback" role="alert">
-                                              <strong>{{ $errors->first('email') }}</strong>
-                                          </span>
-                                      @endif
-                                  </div>
-                          </div>
-                          <div class="form-group row">
-                            <div class="input-group input-group-alternative mb-3">
-                              <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
-                              </div>
-                                          <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder = "Password"name="password" required>
-          
-                                          @if ($errors->has('password'))
-                                              <span class="invalid-feedback" role="alert">
-                                                  <strong>{{ $errors->first('password') }}</strong>
-                                              </span>
-                                          @endif
-                                      </div>
-                              </div>
-                              <div class="form-group row">
-                                <div class="input-group input-group-alternative mb-3">
-                                  <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
-                                  </div>
-                                      <input id="password-confirm" type="password" class="form-control" placeholder = "Confirm Password" name="password_confirmation" required>
-                                          </div>
-                                  </div>
-                                  <div class="form-group row">
-                                    <div class="input-group input-group-alternative mb-3">
-                                      <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-building"></i></span>
-                                      </div>
-                                          <input id="address" type="text" class="form-control" placeholder = "Address" name="address" required>
-                                              </div>
-                                      </div>
-                                      <div class="form-group row">
-                                        <div class="input-group input-group-alternative mb-3">
-                                          <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="ni ni-mobile-button"></i></span>
-                                          </div>
-                                              <input id="tel_no" type="text" class="form-control" placeholder = "Telephone Number" name="tel_no" required>
-                                                  </div>
-                                          </div>
-                                  <div class="form-group row mb-0">
-                                    <div class="col-md-6 offset-md-4">
-                                        <button type="submit" class="btn btn-primary">
-                                            {{ __('Register') }}
-                                        </button>
-                                    </div>
-                                </div>
-                                
-              </form>
-            </div>
+
+<form method="POST" action="{{ route('register') }}">
+        @csrf
+        <div class="card-header text-center">
+          <div class="logo-container">
+            <img src="{{URL::asset('assets/img/logo.png')}}" alt="">
           </div>
         </div>
-      </div>
-    </div>
-  </section>
-  </section>
+        <div class="card-body">
+          <div class="input-group no-border input-lg">
+            <div class="input-group-prepend">
+              <span class="input-group-text">
+                <i class="now-ui-icons users_circle-08"></i>
+              </span>
+            </div>
+            <input id="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder ="Name" name="name" value="{{ old('name') }}" type="text" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+          </div>
+
+          <div class="input-group no-border input-lg">
+            <div class="input-group-prepend">
+              <span class="input-group-text">
+                <i class="now-ui-icons tech_mobile"></i>
+              </span>
+            </div>
+            <input id="tel_no" type="text" class="form-control" placeholder = "Telephone Number" name="tel_no" required>
+
+                                @if ($errors->has('tel_no'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('tel_no') }}</strong>
+                                    </span>
+                                @endif
+          </div>
+
+          <div class="input-group no-border input-lg">
+            <div class="input-group-prepend">
+              <span class="input-group-text">
+                <i class="now-ui-icons location_compass-05"></i>
+              </span>
+            </div>
+            <input id="address" type="text" class="form-control" placeholder = "Address" name="address" required>
+          </div>
+
+          <div class="input-group no-border input-lg">
+            <div class="input-group-prepend">
+              <span class="input-group-text">
+                <i class="now-ui-icons ui-1_email-85"></i>
+              </span>
+            </div>
+            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder ="E-Mail" required>
+
+            @if ($errors->has('email'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
+          </div>
+
+          <div class="input-group no-border input-lg">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">
+                    <i class="now-ui-icons ui-1_lock-circle-open"></i>
+                  </span>
+                </div>
+                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
+
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+              </div>
+
+              <div class="input-group no-border input-lg">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <i class="now-ui-icons ui-1_lock-circle-open"></i>
+                      </span>
+                    </div>
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder= "Confirm Password" required>
+                  </div>
+
+        </div>
+        <div class="card-footer text-center">
+                <button class="btn btn-primary" type="submit">Register</button>
+        </div>
+        <div class="text-center">
+                <h6>
+                  <a href="/login" class="link">Already have an account? Login</a>
+                </h6>
+              </div>
+              
+      </form>
+
 @endsection
+
