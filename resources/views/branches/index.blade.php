56f5052c280a9510b0f0d966b7bb8f552819befa
SanJay <sanjayahashan@live.com>
2019-01-07 23:41:09 +0530

    Branch Image changed

--- resources/views/branches/index.blade.php
+++ resources/views/branches/index.blade.php
@@ -21,7 +21,7 @@
     <center><div class="row">
       <div class="col-md-7">
         <a href="#">
-          <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+          <img class="img-fluid rounded mb-3 mb-md-0" src="{{URL::asset('assets/img/location.jpg')}}" alt="">
         </a>
       </div>
       <div class="col-md-5">
@@ -36,30 +36,6 @@
     <hr>
     @endforeach
 
-    <!-- Pagination -->
-    <ul class="pagination justify-content-center">
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
-          <span aria-hidden="true">&laquo;</span>
-          <span class="sr-only">Previous</span>
-        </a>
-      </li>
-      <li class="page-item">
-        <a class="page-link" href="#">1</a>
-      </li>
-      <li class="page-item">
-        <a class="page-link" href="#">2</a>
-      </li>
-      <li class="page-item">
-        <a class="page-link" href="#">3</a>
-      </li>
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
-          <span aria-hidden="true">&raquo;</span>
-          <span class="sr-only">Next</span>
-        </a>
-      </li>
-    </ul>
 
   </div>
   <!-- /.container -->
